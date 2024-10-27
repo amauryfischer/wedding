1fbb0931bc4d2d23337f348e19d2d92810a4cd46
Amaury <amaufischer@gmail.com>
2024-10-27 21:46:25 +0100

    top bar and todo

---
+++ TODO.md
@@ -0,0 +1,6 @@
+1. connect guest registered to hebergement possible
+2. connect gift list to coda for seeding
+3. add a thank you page
+4. add a gallery page
+5. liste de cadeaux filter
+6. back office
--- src/ui/organisms/Bars/AppBarMenu/AppBarMenu.tsx
+++ src/ui/organisms/Bars/AppBarMenu/AppBarMenu.tsx
@@ -1,13 +1,16 @@
 "use client"
-import { Navbar, NavbarContent } from "@nextui-org/react"
+import { Button, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react"
 import styled from "styled-components"
+import Link from "next/link"
+import { useLocation } from "react-use"
+import { useRouter } from "next/navigation"
 const ResourcesBox = styled.div`
   width: fit-content;
 `
 
 const StyledAppBar = styled(Navbar)`
   padding: 0.25rem;
-  background-color: black !important;
+  background-color: var(--primary);
   position: fixed !important;
   box-shadow: 0px 3px 3px -2px rgb(255 255 255 / 20%),
     0px 3px 4px 0px rgb(255 255 255 / 14%),
@@ -23,9 +26,41 @@ const StyledAppBar = styled(Navbar)`
 `
 
 export default function AppBarMenu() {
+	const location = useLocation()
+	const router = useRouter()
 	return (
-		<StyledAppBar position="static">
-			<NavbarContent>coucou</NavbarContent>
+		<StyledAppBar position="static" className="flex flex-row justify-center">
+			<NavbarContent justify="start" />
+			<NavbarContent className="gap-4" justify="center">
+				<NavbarItem isActive={location.pathname === "/rsvp"}>
+					<Button onPress={() => router.push("/rsvp")} variant="light">
+						<div
+							className={`text-white text-md ${location.pathname === "/rsvp" ? "text-lg font-semibold" : ""}`}
+						>
+							Réservation
+						</div>
+					</Button>
+				</NavbarItem>
+				<NavbarItem isActive={location.pathname === "/gift"}>
+					<Button onPress={() => router.push("/gift")} variant="light">
+						<div
+							className={`text-white text-md ${location.pathname === "/gift" ? "text-lg font-semibold" : ""}`}
+						>
+							Liste de cadeaux
+						</div>
+					</Button>
+				</NavbarItem>
+				<NavbarItem isActive={location.pathname === "/gallery"}>
+					<Button onPress={() => router.push("/gallery")} variant="light">
+						<div
+							className={`text-white text-md ${location.pathname === "/gallery" ? "text-lg font-semibold" : ""}`}
+						>
+							Gallerie photo
+						</div>
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify="end" />
 		</StyledAppBar>
 	)
 }
